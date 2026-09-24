e77dd9e607320d433f441fb4751f2019b5d85c62
dremabuc-web <dremabuc@gmail.com>
2026-09-24 22:04:59 +0800

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,24 @@
+_G.YOKUDO = {
+    -- Hub Info
+    Name = "DRE | Steal An Egg",
+    Version = "Discord: g7uc",
+    Author = "coded by: g7uc | dre",
+    -- Asset
+    AssetID = "rbxassetid://101352576986760",
+
+    -- UI
+    UI = {
+        Width = 480,
+        Height = 340,
+        SidebarWidth = 115,
+        TabHeight = 32,
+        Theme = {
+            Background = Color3.fromRGB(16, 17, 23),
+            Sidebar = Color3.fromRGB(20, 21, 28),
+            TopBar = Color3.fromRGB(23, 24, 32),
+            Accent = Color3.fromRGB(105, 90, 190),
+            Text = Color3.fromRGB(255, 255, 255),
+            SubText = Color3.fromRGB(145, 145, 165),
+        }
+    },
+}
